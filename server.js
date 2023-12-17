3b9286e47a5e1860ec7d7026b106481c6794afca
MercenaryUSA <mercenary.net@outlook.com>
2023-12-17 10:59:33 -0800

    Fixes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 global.__basedir = __dirname;
 
 var corsOptions = {
-  origin: "http://images.mercenaryusa.dev:8081"
+  origin: "http://images.mercenaryusa.dev:81"
 };
 
 app.use(cors(corsOptions));
@@ -15,7 +15,7 @@ const initRoutes = require("./src/routes");
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-let port = 8080;
+let port = 80;
 app.listen(port, () => {
   console.log(`Running on port:${port}`);
 });
